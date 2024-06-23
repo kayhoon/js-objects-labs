46f58984de0d2549d3331808a3f06a473c9f9466
Kayhoon <kevin.huynh21@hotmail.com>
2024-06-23 20:44:20 +1000

    exercise 108

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const pokemon = require('./data.js')
 
-const game = {
-    party: [],
-    gyms: [
-      { location: "Pewter City", completed: false, difficulty: 1 },
-      { location: "Cerulean City", completed: false, difficulty: 2 },
-      { location: "Vermilion City", completed: false, difficulty: 3 },
-      { location: "Celadon City", completed: false, difficulty: 4 },
-      { location: "Fuchsia City", completed: false, difficulty: 5 },
-      { location: "Saffron City", completed: false, difficulty: 6 },
-      { location: "Cinnabar Island", completed: false, difficulty: 7 },
-      { location: "Viridian City", completed: false, difficulty: 8 },
-    ],
-    items: [
-      { name: "potion", quantity: 4 },
-      { name: "pokeball", quantity: 8 },
-      { name: "rare candy", quantity: 99 },
-    ],
-  }
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//   }
 
 
 //   console.dir(pokemon, { maxArrayLength: null })
@@ -163,10 +163,50 @@ const game = {
 
 
 // Solve Exercise 9 here:
-pokemon.forEach (pokemon =>  {
-    if (pokemon.starter === true) {
-        console.log(pokemon)
+
+// pokemon.forEach (pokemon =>  {
+//     if (pokemon.starter === true) {
+//         console.log(pokemon)
+//     }
+// })
+
+// Exercise 10
+// 1. Add a method called `catchPokemon` to the `game` object. This method should:
+//   - Accept an object as a parameter called `pokemonObj`
+//   - Add the `pokemonObj` to the `game.party` array.
+//   - not return anything
+
+// After writing this method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+
+// Solve Exercise 10 here:
+
+const game = {
+    party: [],
+    gyms: [
+      { location: "Pewter City", completed: false, difficulty: 1 },
+      { location: "Cerulean City", completed: false, difficulty: 2 },
+      { location: "Vermilion City", completed: false, difficulty: 3 },
+      { location: "Celadon City", completed: false, difficulty: 4 },
+      { location: "Fuchsia City", completed: false, difficulty: 5 },
+      { location: "Saffron City", completed: false, difficulty: 6 },
+      { location: "Cinnabar Island", completed: false, difficulty: 7 },
+      { location: "Viridian City", completed: false, difficulty: 8 },
+    ],
+    items: [
+      { name: "potion", quantity: 4 },
+      { name: "pokeball", quantity: 8 },
+      { name: "rare candy", quantity: 99 },
+    ],
+    difficulty: [`Hard`],
+    catchPokemon: function(pokemonObj) {
+        this.party.push(pokemonObj)
     }
-})
+}
+
+game.catchPokemon(pokemon[134])
+console.log(game.party)
+
+
+
 
 
