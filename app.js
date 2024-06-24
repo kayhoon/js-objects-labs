5a9b9c4ba19b50fbac6ee822958be747e446ac64
Kayhoon <kevin.huynh21@hotmail.com>
2024-06-24 17:27:58 +1000

    exercise 12

--- app.js
+++ app.js
@@ -180,6 +180,43 @@ const pokemon = require('./data.js')
 
 // Solve Exercise 10 here:
 
+// const game = {
+//     party: [],
+//     gyms: [
+//       { location: "Pewter City", completed: false, difficulty: 1 },
+//       { location: "Cerulean City", completed: false, difficulty: 2 },
+//       { location: "Vermilion City", completed: false, difficulty: 3 },
+//       { location: "Celadon City", completed: false, difficulty: 4 },
+//       { location: "Fuchsia City", completed: false, difficulty: 5 },
+//       { location: "Saffron City", completed: false, difficulty: 6 },
+//       { location: "Cinnabar Island", completed: false, difficulty: 7 },
+//       { location: "Viridian City", completed: false, difficulty: 8 },
+//     ],
+//     items: [
+//       { name: "potion", quantity: 4 },
+//       { name: "pokeball", quantity: 8 },
+//       { name: "rare candy", quantity: 99 },
+//     ],
+//     difficulty: [`Hard`],
+//     catchPokemon: function(pokemonObj) {
+//         this.party.push(pokemonObj)
+//     }
+// }
+
+// game.catchPokemon(pokemon[134])
+// console.log(game.party)
+
+// Exercise 11
+// 1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+// 2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+// Tips:
+// For this exercise, it's okay to have a negative number of pokeballs.
+// After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+// Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+// Solve Exercise 11 here:
+
 const game = {
     party: [],
     gyms: [
@@ -203,10 +240,23 @@ const game = {
     }
 }
 
-game.catchPokemon(pokemon[134])
-console.log(game.party)
 
 
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+game.gyms.forEach( gym => {
+    if (gym.difficulty < 6) {
+        gym.completed = true
+    }
+})
+
+console.log(game.gyms)
 
 
 
